37b7e0696f9f6a74a29c77b94a672127f2be8edc
ronish <ronish@ronish>
2023-03-11 22:22:46 +0100

    Added file name and added image libraries

--- dicom2xml/src/main/java/dicom2xml/main/DicomToXml.java
+++ dicom2xml/src/main/java/dicom2xml/main/DicomToXml.java
@@ -2,10 +2,7 @@ package dicom2xml.main;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.UUID;
 
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.sax.SAXTransformerFactory;
@@ -13,8 +10,10 @@ import javax.xml.transform.sax.TransformerHandler;
 import javax.xml.transform.stream.StreamResult;
 
 import org.dcm4che3.data.Attributes;
+import org.dcm4che3.data.Tag;
 import org.dcm4che3.io.DicomInputStream;
 import org.dcm4che3.io.SAXWriter;
+import org.dcm4che3.image.ICCProfile;
 
 /**
  * This class contains the main function of the tool.
@@ -29,7 +28,9 @@ public class DicomToXml {
 	public static void main(final String[] args) {
 		try {
 			final File outputFolder = createEmptyDirectory("/home/ronish/DevTools/Temp/out1");
-			createXMLFromDICOMFile(new File("/home/ronish/DevTools/Temp/color-1.dcm"), outputFolder);
+			final File inputDicomFile = new File("/home/ronish/DevTools/Temp/color-1.dcm");
+			final String filePrefix = getOutputFileNamePrefix(inputDicomFile);
+			createXMLFromDICOMFile(inputDicomFile, outputFolder, filePrefix);
 		} catch (TransformerConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -39,28 +40,43 @@ public class DicomToXml {
 		}
 	}
 
-	public static void createXMLFromDICOMFile(final File inputDCMFile, final File outputFolder)
-			throws IOException, TransformerConfigurationException {
+	public static void createXMLFromDICOMFile(final File inputDCMFile, final File outputFolder,
+			final String outputFilePrefix) throws IOException, TransformerConfigurationException {
 
-		final String tempFileName = UUID.randomUUID().toString();
-		final File tempOutPutFile = new File(outputFolder, tempFileName);
+		final File xmlOutputFile = new File(outputFolder, String.format("%s.xml", outputFilePrefix));
 		final TransformerHandler transformerHandler = getTransformerHandler();
 
-		transformerHandler.setResult(new StreamResult(tempOutPutFile));
+		transformerHandler.setResult(new StreamResult(xmlOutputFile));
 
 		final DicomInputStream dicomInputStream = new DicomInputStream(inputDCMFile);
 		final SAXWriter saxWriter = new SAXWriter(transformerHandler);
 		saxWriter.setIncludeNamespaceDeclaration(true);
 		dicomInputStream.setDicomInputHandler(saxWriter);
+		dicomInputStream.readDataset();
+		dicomInputStream.close();
+	}
+	
+	public static void createJPEGFromDICOMFile(final File inputDCMFile, final File outputFolder,
+			final String outputFilePrefix) throws IOException, TransformerConfigurationException {
+		//final ICCProfi
+		final File jpegOutputFile = new File(outputFolder, String.format("%s.jpeg", outputFilePrefix));
+		
+	}
+
+	public static String getOutputFileNamePrefix(final File inputDCMFile)
+			throws IOException, TransformerConfigurationException {
+		final DicomInputStream dicomInputStream = new DicomInputStream(inputDCMFile);
 		final Attributes attributes = dicomInputStream.readDataset();
 		dicomInputStream.close();
+		return String.format("%s_%s", attributes.getString(Tag.PatientName), attributes.getString(Tag.PatientID));
 	}
 
 	private static File createEmptyDirectory(final String folderPath) {
 		final File folder = new File(folderPath);
 		if (folder.exists()) {
 			throw new IllegalArgumentException(String.format(
-					"Folder %s already exists, use a different folder or delete the folder and try again.", folderPath));
+					"Folder %s already exists, use a different folder or delete the folder and try again.",
+					folderPath));
 		}
 		folder.mkdirs();
 		return folder;
