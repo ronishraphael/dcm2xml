c68f6ccad027685834fd546066ac93846afe7831
ronish <ronish@ronish>
2023-03-12 15:53:22 +0100

    Added JPEG extraction logic.

--- dicom2xml/src/main/java/dicom2xml/main/DicomToXml.java
+++ dicom2xml/src/main/java/dicom2xml/main/DicomToXml.java
@@ -1,19 +1,26 @@
 package dicom2xml.main;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.FileImageOutputStream;
+import javax.imageio.stream.ImageInputStream;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.sax.SAXTransformerFactory;
 import javax.xml.transform.sax.TransformerHandler;
 import javax.xml.transform.stream.StreamResult;
 
+import org.dcm4che2.data.ConfigurationError;
 import org.dcm4che3.data.Attributes;
 import org.dcm4che3.data.Tag;
 import org.dcm4che3.io.DicomInputStream;
 import org.dcm4che3.io.SAXWriter;
-import org.dcm4che3.image.ICCProfile;
 
 /**
  * This class contains the main function of the tool.
@@ -28,9 +35,10 @@ public class DicomToXml {
 	public static void main(final String[] args) {
 		try {
 			final File outputFolder = createEmptyDirectory("/home/ronish/DevTools/Temp/out1");
-			final File inputDicomFile = new File("/home/ronish/DevTools/Temp/color-1.dcm");
+			final File inputDicomFile = new File("/home/ronish/DevTools/Temp/color-symbol.dcm");
 			final String filePrefix = getOutputFileNamePrefix(inputDicomFile);
 			createXMLFromDICOMFile(inputDicomFile, outputFolder, filePrefix);
+			createJPEGFromDICOMFile(inputDicomFile, outputFolder, filePrefix);
 		} catch (TransformerConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -58,9 +66,41 @@ public class DicomToXml {
 	
 	public static void createJPEGFromDICOMFile(final File inputDCMFile, final File outputFolder,
 			final String outputFilePrefix) throws IOException, TransformerConfigurationException {
-		//final ICCProfi
-		final File jpegOutputFile = new File(outputFolder, String.format("%s.jpeg", outputFilePrefix));
-		
+		final String imageType = "JPEG";
+		final ImageReader imageReader = ImageIO.getImageReadersByFormatName("DICOM").next();
+		final ImageInputStream imageInputStream = ImageIO.createImageInputStream(inputDCMFile);
+		imageReader.setInput(imageInputStream);
+		int counter = 0;
+		while (true) {
+
+			final BufferedImage bufferedImage;
+			try {
+				bufferedImage = imageReader.read(counter++);
+			} catch (final IOException exp) {
+				//TODO Find a better way to exit.
+				return;
+			} catch (ConfigurationError ce) {
+				//TODO Find a better way to exit in no JPEGs.
+				return;
+			}
+			final ImageWriter imageWriter = ImageIO.getImageWritersByFormatName(imageType).next();
+			final File jpegOutputFile = new File(outputFolder,
+					String.format("%s_%s.%s", outputFilePrefix, counter, imageType));
+			final RandomAccessFile raf = new RandomAccessFile(jpegOutputFile, "rw");
+			raf.setLength(0);
+			final FileImageOutputStream fileImageOutputStream = new FileImageOutputStream(raf);
+			imageWriter.setOutput(fileImageOutputStream);
+			while (true) {
+				try {
+					imageWriter.write(bufferedImage);
+				} catch (Exception exp) {
+					exp.printStackTrace();
+					break;
+				}
+			}
+			fileImageOutputStream.close();
+			imageInputStream.close();
+		}
 	}
 
 	public static String getOutputFileNamePrefix(final File inputDCMFile)
